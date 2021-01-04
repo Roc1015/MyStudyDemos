5b3c24ff98d0a9d8c8b8d2710c8c20444dfdbdab
WP <1344471553@qq.com>
2021-01-04 10:40:06 +0800

    Frist commit and push

---
+++ java-basic-study/src/main/java/com/roc/study/basicKnowledge/hello.java
@@ -0,0 +1,11 @@
+package com.roc.study.basicKnowledge;
+
+/**
+ * @author WP
+ * @date 2021/1/4  10:32
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello，world");
+    }
+}
